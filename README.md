8670d2ee8dbc8920d97564ab8767bd28b50085bf
ZiTe <honmonoh@gmail.com>
2022-04-25 21:14:21 +0800

    Add code files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# stm32-control-dynamixel-motor
-Control ROBOTIS DYNAMIXEL motor with STM32 and LibOpenCM3
+# STM32 Control DYNAMIXEL Motor
+
+Control ROBOTIS DYNAMIXEL motor with MAX485(RS-485 module), STM32 NUCLEO-F446RE and [LibOpenCM3](https://libopencm3.org/).
+
+Tested on [H42-20-S300-R](https://emanual.robotis.com/docs/en/dxl/pro/h42-20-s300-r/) and [H54-200-S500-R](https://emanual.robotis.com/docs/en/dxl/pro/h54-200-s500-r/).
+
+- Example code: [/src/main.c](/src/main.c)  
+- [DYNAMIXEL Protocol 2.0](https://emanual.robotis.com/docs/en/dxl/protocol2/) library files: [/lib/DYNAMIXEL/](/lib/DYNAMIXEL/)
+
+## Pin map
+
+| NUCLEO-F446RE Board  | MAX485    |
+| -------------------- | --------- |
+| D3 (PB3)             | DE and RE |
+| D2 (PA10, USART1_RX) | RO        |
+| D8 (PA9, USART1_TX)  | DI        |
+
+| DYNAMIXEL Connector | MAX485 |
+| ------------------- | ------ |
+| 1, GND              | --     |
+| 2, VDD              | --     |
+| 3, DATA+            | A      |
+| 4, DATA-            | B      |
+
+> Ref: [Communication Circuit](https://emanual.robotis.com/docs/en/dxl/pro/h42-20-s300-r/#communication-circuit)
---
+++ src/main.c
@@ -0,0 +1,191 @@
+/**
+ * @file   main.cpp
+ * @author ZiTe (honmonoh@gmail.com)
+ * @brief  Control DYNAMIXEL motor with RS-485 UART, using MAX485.
+ */
+
+#include "dynamixel_2_0.h"
+#include "printf.h"
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/adc.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/nvic.h>
+
+#define ID_1 (10)
+#define ID_2 (6)
+
+/* PA5 = D13, User-LED. */
+#define GPIO_LED_PORT (GPIOA)
+#define GPIO_LED_PIN (GPIO5)
+
+/* PB3 = D3, MAX485 DE and RE pin. */
+#define GPIO_ENABLE_PORT (GPIOB)
+#define GPIO_ENABLE_PIN (GPIO3)
+
+#define DYNAMIXEL_UART_BAUD_RATE (57600)
+#define DYNAMIXEL_USART (USART1)
+#define DYNAMIXEL_USART_IRQ (NVIC_USART1_IRQ)
+
+/* PA9 = D8, USART1_TX, MAX485 DI pin. */
+#define GPIO_DYNAMIXEL_UART_TX_PORT (GPIOA)
+#define GPIO_DYNAMIXEL_UART_TX_PIN (GPIO9)
+
+/* PA10 = D2, USART1_RX, MAX485 RO pin. */
+#define GPIO_DYNAMIXEL_UART_RX_PORT (GPIOA)
+#define GPIO_DYNAMIXEL_UART_RX_PIN (GPIO10)
+
+#define CONSOLE_UART_BAUD_RATE (9600)
+#define CONSOLE_USART (USART2)
+#define CONSOLE_USART_IRQ (NVIC_USART2_IRQ)
+
+#define GPIO_CONSOLE_UART_TX_PORT (GPIOA)
+#define GPIO_CONSOLE_UART_TX_PIN (GPIO2)
+
+#define GPIO_CONSOLE_UART_RX_PORT (GPIOA)
+#define GPIO_CONSOLE_UART_RX_PIN (GPIO3)
+
+#define BUFFER_LENGTH (128)
+
+uint8_t buffer[BUFFER_LENGTH] = {0};
+volatile uint16_t buffer_index = 0;
+
+void rcc_setup(void);
+void console_usart_setup(void);
+void dynamixel_usart_setup(void);
+void led_setup(void);
+void other_gpio_setup(void);
+void delay(volatile uint64_t value);
+
+int main(void)
+{
+  rcc_setup();
+  led_setup();
+  other_gpio_setup();
+  console_usart_setup();
+  dynamixel_usart_setup();
+
+  dynamixel2_set_torque_enable(ID_1, true);
+  dynamixel2_set_torque_enable(ID_2, true);
+  delay(100000);
+
+  dynamixel2_clear_receive_buffer();
+  printf("Ready\r\n");
+
+  int32_t goal_position = 10000;
+  while (1)
+  {
+    dynamixel2_set_goal_position(ID_1, goal_position);
+    delay(50000);
+    dynamixel2_set_goal_position(ID_2, goal_position);
+    delay(10000000);
+
+    uint32_t p1 = dynamixel2_read_present_position(ID_1);
+    delay(50000);
+    uint32_t p2 = dynamixel2_read_present_position(ID_2);
+    printf("ID%i: %li.ID%i: %li\r\n", ID_1, p1, ID_2, p2);
+    delay(10000000);
+
+    gpio_toggle(GPIO_LED_PORT, GPIO_LED_PIN);
+
+    if (goal_position == 10000)
+    {
+      goal_position = -goal_position;
+    }
+    else
+    {
+      goal_position = 10000;
+    }
+  }
+
+  return 0;
+}
+
+void rcc_setup(void)
+{
+  rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+
+  rcc_periph_clock_enable(RCC_GPIOA);
+  rcc_periph_clock_enable(RCC_GPIOB);
+  rcc_periph_clock_enable(RCC_USART1);
+  rcc_periph_clock_enable(RCC_USART2);
+}
+
+void console_usart_setup(void)
+{
+  /* Tx. */
+  gpio_mode_setup(GPIO_CONSOLE_UART_TX_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_CONSOLE_UART_TX_PIN);
+  gpio_set_af(GPIO_CONSOLE_UART_TX_PORT, GPIO_AF7, GPIO_CONSOLE_UART_TX_PIN);
+
+  /* Rx. */
+  gpio_mode_setup(GPIO_CONSOLE_UART_RX_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_CONSOLE_UART_RX_PIN);
+  gpio_set_af(GPIO_CONSOLE_UART_RX_PORT, GPIO_AF7, GPIO_CONSOLE_UART_RX_PIN);
+
+  nvic_enable_irq(CONSOLE_USART_IRQ);
+
+  usart_set_baudrate(CONSOLE_USART, CONSOLE_UART_BAUD_RATE);
+  usart_set_databits(CONSOLE_USART, 8);
+  usart_set_stopbits(CONSOLE_USART, USART_STOPBITS_1);
+  usart_set_parity(CONSOLE_USART, USART_PARITY_NONE);
+  usart_set_flow_control(CONSOLE_USART, USART_FLOWCONTROL_NONE);
+  usart_set_mode(CONSOLE_USART, USART_MODE_TX_RX);
+
+  usart_enable_rx_interrupt(CONSOLE_USART);
+
+  usart_enable(CONSOLE_USART);
+}
+
+void dynamixel_usart_setup(void)
+{
+  /* Tx. */
+  gpio_mode_setup(GPIO_DYNAMIXEL_UART_TX_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_DYNAMIXEL_UART_TX_PIN);
+  gpio_set_af(GPIO_DYNAMIXEL_UART_TX_PORT, GPIO_AF7, GPIO_DYNAMIXEL_UART_TX_PIN);
+
+  /* Rx. */
+  gpio_mode_setup(GPIO_DYNAMIXEL_UART_RX_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_DYNAMIXEL_UART_RX_PIN);
+  gpio_set_af(GPIO_DYNAMIXEL_UART_RX_PORT, GPIO_AF7, GPIO_DYNAMIXEL_UART_RX_PIN);
+
+  nvic_enable_irq(DYNAMIXEL_USART_IRQ);
+
+  usart_set_baudrate(DYNAMIXEL_USART, DYNAMIXEL_UART_BAUD_RATE);
+  usart_set_databits(DYNAMIXEL_USART, 8);
+  usart_set_stopbits(DYNAMIXEL_USART, USART_STOPBITS_1);
+  usart_set_parity(DYNAMIXEL_USART, USART_PARITY_NONE);
+  usart_set_flow_control(DYNAMIXEL_USART, USART_FLOWCONTROL_NONE);
+  usart_set_mode(DYNAMIXEL_USART, USART_MODE_TX_RX);
+
+  usart_enable_rx_interrupt(DYNAMIXEL_USART);
+
+  usart_enable(DYNAMIXEL_USART);
+}
+
+void led_setup(void)
+{
+  gpio_mode_setup(GPIO_LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_LED_PIN);
+  gpio_set_output_options(GPIO_LED_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_LED_PIN);
+
+  gpio_clear(GPIO_LED_PORT, GPIO_LED_PIN); /* LED off. */
+}
+
+void other_gpio_setup(void)
+{
+  gpio_mode_setup(GPIO_ENABLE_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_ENABLE_PIN);
+  gpio_set_output_options(GPIO_ENABLE_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, GPIO_ENABLE_PIN);
+}
+
+void delay(volatile uint64_t value)
+{
+  while (value--)
+  {
+    __asm__("nop"); /* Do nothing. */
+  }
+}
+
+void usart1_isr(void)
+{
+  if ((USART_SR(DYNAMIXEL_USART) & USART_SR_RXNE) != 0)
+  {
+    uint8_t indata = usart_recv(DYNAMIXEL_USART);
+    dynamixel2_receive_callback(indata);
+  }
+}
